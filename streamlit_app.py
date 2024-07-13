1787a3b5ec1156cf8424bef14c4e889786c43e54
jhgf342 <dluc3994@gmail.com>
2024-07-13 22:07:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,9 +123,9 @@ def main():
 
                 st.header("Latest News")
                 for i, article in enumerate(news_articles):
-                    st.markdown("""
+                    st.markdown(""
                         <div class="news-article">
-                            <h2>{i+1}. {article['title']}</h2>
+                            <h2>{{i+1}}. {article['title']}</h2>
                             <p>{article['description']}</p>
                             <a href="{article['url']}" target="_blank">Read more</a>
                             </div> 
